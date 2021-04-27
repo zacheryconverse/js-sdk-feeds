2211aa4d1f719a86c2dcf01c6ca315d4b1378d00
Zachery Converse <zacheryconverse@gmail.com>
2021-04-27 13:29:54 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -9,18 +9,8 @@ const key = process.env['REACT_APP_KEY'];
 const appID = process.env["REACT_APP_ID"];
 
 function App() {
-  // const [token, setToken] = useState('');
   const [view, setView] = useState('login');
   const [client, setClient] = useState("");
-
-  // useEffect(() => {
-  //   axios.post('http://localhost:8000/token', {})
-  //   // .then((res) => setToken(res.data))
-  //   .catch((err) => console.error(err));
-  // }, []);
-
-  // const client = stream.connect(key, token, appID);
-  // console.log('CLIENT', client);
 
   return (
     <div className="App">
