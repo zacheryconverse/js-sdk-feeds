80c88a59cf20d9c2916390ac5c1ed3e1ec88b271
Zachery Converse <zacheryconverse@gmail.com>
2021-05-21 13:45:18 -0600

    fix warning. render seen posts with flag

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,28 +1,26 @@
 export default function Notification({ notification }) {
-    console.log(notification)
-    const renderNotifs = () => {
-       return notification.activities.map(not => {
-            return <> {not.text} </>
-        })
-    }
+  console.log(notification);
+  const renderNotifs = () => {
+    return notification.activities.map((not) => {
+      return <> {not.text} </>;
+    });
+  };
 
-    if (!notification.is_seen) {
-        return <div style={activityContainer}>{renderNotifs()}</div>
-    } else {
-        return "gi"
-    }
-
+  if (!notification.is_seen) {
+    return <div style={activityContainer}>{renderNotifs()}</div>;
+  } else {
+    return <div style={activityContainer}>SEEN!--{renderNotifs()}--SEEN!</div>;
+  }
 }
 
 const activityContainer = {
-    display: "flex",
-    background:
-      "linear-gradient(to right, rgba(0, 151, 221, 100), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(0, 151, 221, 50))",
-    borderBottom: "1px solid grey",
-    color: "black",
-    width: "75vw",
-    margin: "10px",
-    padding: "25px 50px",
-    justifyContent: "center",
-  };
-  
+  display: "flex",
+  background:
+    "linear-gradient(to right, rgba(0, 151, 221, 100), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(255, 255, 255, 50), rgba(0, 151, 221, 50))",
+  borderBottom: "1px solid grey",
+  color: "black",
+  width: "75vw",
+  margin: "10px",
+  padding: "25px 50px",
+  justifyContent: "center",
+};
--- src/components/NotificationList/NotificationList.js
+++ src/components/NotificationList/NotificationList.js
@@ -1,11 +1,12 @@
-import Notification from '../Notification/Notification'
+import Notification from "../Notification/Notification";
 export default function NotificationList({ notifications }) {
-    if (notifications) {
-        return notifications.results.map(notification => {
-            return <Notification notification={notification}/>
-        })
-    } else {
-        return ''
-    }
-
+  if (notifications) {
+    return notifications.results.map((notification) => {
+      return (
+        <Notification key={notification.duration} notification={notification} />
+      );
+    });
+  } else {
+    return "";
+  }
 }
